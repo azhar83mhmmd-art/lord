cfab2888a9e6f4faaa843d93a044951966e9f2d7
azhar83mhmmd-art <azhar83mhmmd@gmail.com>
2026-07-17 10:37:28 +0700

    Hapus logger.js

--- middleware/logger.js
+++
@@ -1,7 +0,0 @@
-function logger(req, res, next) {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] ${req.method} ${req.originalUrl}`);
-  next();
-}
-
-module.exports = logger;
