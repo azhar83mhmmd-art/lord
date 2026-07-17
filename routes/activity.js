1162bd90d1bfe3e1266fcd4b910ca2996eb0a18a
azhar83mhmmd-art <azhar83mhmmd@gmail.com>
2026-07-17 10:39:25 +0700

    Hapus activity.js

--- routes/activity.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getActivities } = require('../controllers/activityController');
-
-router.get('/', getActivities);
-
-module.exports = router;
